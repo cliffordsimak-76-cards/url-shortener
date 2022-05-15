00c1722fef112e31e107305e04cf55fa38aadd79
CliffordSimak <holamellamosasha@gmail.com>
2022-05-15 12:29:20 +0300

    add port validating

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/caarlos0/env"
 	"github.com/cliffordsimak-76-cards/url-shortener/internal/app"
 	"github.com/cliffordsimak-76-cards/url-shortener/internal/app/config"
 	"log"
 	"os"
+	"regexp"
+	"strings"
 )
 
 func main() {
-	cfg := setConfig()
-	if err := app.Run(cfg); err != nil {
+	cfg, err := setConfig()
+	if err != nil {
+		log.Fatal("error running server: ", err)
+	}
+
+	if err = app.Run(cfg); err != nil {
 		log.Fatal("error running server", err)
 	}
 }
 
-func setConfig() *config.Config {
+func setConfig() (*config.Config, error) {
 	cfg := &config.Config{}
 	if err := env.Parse(cfg); err != nil {
 		log.Fatalf("failed to retrieve env variables, %v", err)
@@ -24,10 +31,15 @@ func setConfig() *config.Config {
 	if baseURL, ok := os.LookupEnv("BASE_URL"); ok {
 		cfg.BaseURL = baseURL
 	}
-
 	if serverAddress, ok := os.LookupEnv("SERVER_ADDRESS"); ok {
 		cfg.ServerAddress = serverAddress
 	}
 
-	return cfg
+	baseURLPort := strings.Split(cfg.BaseURL, ":")[2]
+	ok, _ := regexp.MatchString(cfg.PortPattern, baseURLPort)
+	if !ok {
+		return nil, fmt.Errorf("base url port has wrong format %s", baseURLPort)
+	}
+
+	return cfg, nil
 }
--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -2,5 +2,6 @@ package config
 
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:":8080"`
-	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080/"`
+	BaseURL       string `env:"BASE_URL" envDefault:"http://localhost:8080"`
+	PortPattern   string `envDefault:"^([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])$"`
 }
