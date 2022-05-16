f0800e2d0d09b3ed804d3a6bc781842f410109ae
CliffordSimak <holamellamosasha@gmail.com>
2022-05-16 22:17:07 +0300

    parse flags

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/cliffordsimak-76-cards/url-shortener/internal/app"
 	"github.com/cliffordsimak-76-cards/url-shortener/internal/app/config"
 	"log"
@@ -12,7 +13,16 @@ func main() {
 		log.Fatal("error running server: ", err)
 	}
 
+	parseFlags(cfg)
+
 	if err = app.Run(cfg); err != nil {
 		log.Fatal("error running server", err)
 	}
+}
+
+func parseFlags(cfg *config.Config) {
+	flag.StringVar(&cfg.ServerAddress, "a", cfg.ServerAddress, "address to listen on")
+	flag.StringVar(&cfg.BaseURL, "b", cfg.BaseURL, "base URL for short link")
+	flag.StringVar(&cfg.FileStoragePath, "f", cfg.FileStoragePath, "file storage path")
+	flag.Parse()
 }
