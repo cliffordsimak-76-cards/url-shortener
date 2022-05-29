7aa7d7f72cb9f05e5fa5a40e9f72720e18acf408
CliffordSimak <holamellamosasha@gmail.com>
2022-05-30 00:04:47 +0300

    Update get_all.go

--- internal/app/httphandlers/get_all.go
+++ internal/app/httphandlers/get_all.go
@@ -17,14 +17,18 @@ func (h *HTTPHandler) GetAll(c echo.Context) error {
 		return c.String(http.StatusBadRequest, err.Error())
 	}
 
-	urls, err := h.repository.GetAll(userID)
+	urlModels, err := h.repository.GetAll(userID)
 	if err != nil {
 		log.Error(err)
 		return c.String(http.StatusBadRequest, "error get all")
 	}
 
+	if len(urlModels) == 0 {
+		return c.NoContent(http.StatusNoContent)
+	}
+
 	var response []*URL
-	for _, url := range urls {
+	for _, url := range urlModels {
 		response = append(response, &URL{
 			Short:    h.buildURL(url.Short),
 			Original: url.Original,
